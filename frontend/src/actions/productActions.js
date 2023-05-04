0edb4863a2640d11ae810e1a37b8cd86448e7005
pinballtec <daniilboiko99@gmail.com>
2023-05-04 20:07:04 +0200

    refractor of workflow + lint for frontend

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,27 +1,29 @@
-import axios from 'axios'
+import axios from "axios";
 import {
-    PRODUCT_LIST_FAIL,
-    PRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_SUCCESS
-} from '../constants/productConstants'
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+} from "../constants/productConstants";
 
 let listProducts = () => async (dispatch) => {
-    try{
-        dispatch({type:PRODUCT_LIST_REQUEST})
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
 
-        const {data} = await axios.get('http://127.0.0.1:8000/api/products/')
+    const { data } = await axios.get("http://127.0.0.1:8000/api/products/");
 
-        dispatch({
-            type: PRODUCT_LIST_SUCCESS,
-            payload: data
-        })
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-    }catch(error){
-        dispatch({
-            type:PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-        })
-    }
-}
+export default listProducts;
